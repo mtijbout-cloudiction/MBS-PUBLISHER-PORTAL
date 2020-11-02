3dbcacadab8893b885071a5167d9e09143fc0272
Marco Tijbout (@Cloudiction) <72575640+mtijbout-cloudiction@users.noreply.github.com>
2020-11-02 18:26:10 +0100

    [ Updates ... ]

--- STATEMENT/main.sh
+++ STATEMENT/main.sh
@@ -50,8 +50,8 @@ do
     echo -e "\n- Install delploy.sh ..."
     ssh ${ADMIN}@$i sudo /home/cloudiction/scripts/installDeploy.sh
 
-    # echo -e "\n- Add NVM to PATH ..."
-    # ssh ${ADMIN}@$i sudo /home/cloudiction/scripts/modEnv.sh
+    echo -e "\n- Add NVM to PATH ..."
+    ssh ${ADMIN}@$i sudo /home/cloudiction/scripts/modEnv.sh
 
     # echo -e "\n- Enable usage of ~/.ssh/environment file ..."
     # ssh ${ADMIN}@$i sudo /home/cloudiction/scripts/enableSSHEnv.sh
