342db2744a32842cd8ba28ed1c24ad9d365a7d7a
Marco Tijbout (@Cloudiction) <72575640+mtijbout-cloudiction@users.noreply.github.com>
2021-01-22 13:19:29 +0100

    Move to correct project

--- STATEMENT/codedeploy/MoveSource.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# This script is part of the package that is managed by 4NET
-
-# Copy content source directory to versions
-cp -r $SOURCE_DIR/. ${VERSIONS_DIR}/${DEPL_VER}/
--- STATEMENT/codedeploy/Start.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# This script is part of the package that is managed by 4NET
-
-# Reload pm2
-cd $CURRENT_LINK
-su gitlab -c "pm2 reload ecosystem.config.js;";
